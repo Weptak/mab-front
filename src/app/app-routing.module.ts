490da4087c6aefafa128d45244b3dfb061b4dd0d
Yorick Weenen <71253572+Weptak@users.noreply.github.com>
2020-12-03 16:40:45 +0100

    Routing update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AllArtefactsComponent } from './Components/all-artefacts/all-artefacts.component';
+import { ArtefactsFromCultureComponent } from './Components/artefacts-from-culture/artefacts-from-culture.component';
+import { CollectionsComponent } from './Components/collections/collections.component';
+import { ExpositionsComponent } from './Components/expositions/expositions.component';
 import { HomePageComponent } from './Components/home-page/home-page.component';
+import { OldExpositionsComponent } from './Components/old-expositions/old-expositions.component';
 
 const routes: Routes = [
   { path: 'welcome', component: HomePageComponent },
   { path: '', pathMatch: 'full', redirectTo: 'welcome' },
+  { path: 'expos', component: ExpositionsComponent },
+  { path: 'expos/old', component: OldExpositionsComponent },
+  {
+    path: 'collections',
+    component: CollectionsComponent,
+    children: [
+      { path: '', component: AllArtefactsComponent },
+      { path: ':id', component: ArtefactsFromCultureComponent },
+    ],
+  },
 ];
 
 @NgModule({
