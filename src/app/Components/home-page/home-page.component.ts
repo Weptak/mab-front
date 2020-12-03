6d2363b3d5910dfbb2b1eaa9d7fd3f808cc6fea4
Yorick Weenen <71253572+Weptak@users.noreply.github.com>
2020-12-03 16:38:50 +0100

    welcome page expo and presentation

--- src/app/Components/home-page/home-page.component.ts
+++ src/app/Components/home-page/home-page.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { IExposition } from 'src/app/domain/iexposition';
+import { ExpositionService } from 'src/app/services/exposition.service';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+  styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent implements OnInit {
+  currentExpos: IExposition[];
 
-  constructor() { }
+  constructor(private _expoService: ExpositionService) {}
 
   ngOnInit(): void {
+    this._expoService.getAllCurrentExpos(0, 3).subscribe(
+      (resp) => {
+        this.currentExpos = resp.content;
+      },
+      (err) => console.log(`Error retrieving current expositions : ${err}`)
+    );
   }
-
 }
