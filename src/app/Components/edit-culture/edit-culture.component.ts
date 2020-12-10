04637cbc49c66f1bf2a8b64e0d686b0ed691a017
Yorick Weenen <71253572+Weptak@users.noreply.github.com>
2020-12-10 16:27:39 +0100

    edit culture update : using form

--- src/app/Components/edit-culture/edit-culture.component.ts
+++ src/app/Components/edit-culture/edit-culture.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ICulture } from 'src/app/domain/iculture';
 import { CultureService } from 'src/app/services/culture.service';
@@ -11,39 +17,43 @@ import { CultureService } from 'src/app/services/culture.service';
 export class EditCultureComponent implements OnInit {
   culture: ICulture;
   cultureID: string;
-  isValid: boolean = true;
+  form: FormGroup;
 
   constructor(
     private _cultureService: CultureService,
     private _route: ActivatedRoute,
-    private _router: Router
+    private _router: Router,
+    private _formBuilder: FormBuilder
   ) {}
 
   ngOnInit(): void {
     this._route.paramMap.subscribe((resp) => {
       this.cultureID = resp.get('id');
       this._cultureService.getCultureById(+this.cultureID).subscribe(
-        (res) => (this.culture = res),
+        (res) => {
+          this.culture = res;
+          this.form = this._formBuilder.group({
+            id: new FormControl(this.culture.id, Validators.required),
+            name: new FormControl(this.culture.name, Validators.required),
+            description: new FormControl(this.culture.description),
+            periodDescription: new FormControl(this.culture.periodDescription),
+            cultureMap: new FormControl(this.culture.cultureMap),
+            startYear: new FormControl(this.culture.startYear),
+            endYear: new FormControl(this.culture.endYear),
+          });
+        },
         (err) => console.log('Error retrieving the culture : ' + err)
       );
     });
   }
 
-  check() {
-    if (this.culture.name != null && this.culture.name != '') {
-      this.isValid = true;
-    }
-  }
-
   public editCulture(culture: ICulture) {
-    if (culture.name != null && culture.name != '') {
-      this._cultureService.editCulture(culture).subscribe((resp) => {
-        console.log('Culture update ' + culture.name);
-        this._router.navigate(['/collections/' + this.cultureID]);
+    this._cultureService.editCulture(culture).subscribe((resp) => {
+      console.log('Culture update ' + culture.name);
+      this._router.navigate(['/collections/' + this.cultureID]).then(() => {
+        window.location.reload();
       });
-    } else {
-      this.isValid = false;
-    }
+    });
   }
   public back() {
     this._router.navigate(['/collections/' + this.cultureID]);
